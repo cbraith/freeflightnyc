133153f9876ce749b7c21bb25c0949fec9377b7b
Chris Braithwaite <chris@gasolinedigital.com>
2013-04-21 12:14:07 -0400

    adding nyc boundary

--- app/js/directives.js
+++ app/js/directives.js
@@ -12,14 +12,18 @@ angular.module('myApp.directives', []).
   directive('flightMap', [function () {
     return function (scope, el, attrs) {
         var mapOptions = {
-            center: new google.maps.LatLng(-34.397, 150.644),
+            center: new google.maps.LatLng(41.5784, -73.364661),
             zoom: 8,
             mapTypeId: google.maps.MapTypeId.ROADMAP
         };
 
         console.log('directive:', scope);
         var map = new google.maps.Map(document.getElementById("flight-map"),
-            mapOptions);
+            mapOptions),
+            nyBostonBounds = map.getBounds();
+
+        console.log('NYC/Boston Boundary', nyBostonBounds);
+
     }
-    }]);
+}]);
 
